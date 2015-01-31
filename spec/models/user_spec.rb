edf0c98499ef15058ad5b4320187dcee2eb545c7
Daniel Glunz <dannyglunz@gmail.com>
2015-01-31 13:15:07 -0700

    update gems, add more user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe User do
+  it { should have_secure_password }
   it { should validate_presence_of(:email_address) }
   it { should validate_uniqueness_of(:email_address) }
+  it { should validate_presence_of(:password) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should have_many(:inner_circles) }
+  it { should have_and_belong_to_many(:followers) }
+  it { should have_and_belong_to_many(:owners) }
 
   it 'should downcase the user email before saving' do
     user = build :user, email_address: "UNCLeUnI@gmail.com"
@@ -12,3 +18,4 @@ describe User do
     expect(user.email_address).to eq "uncleuni@gmail.com"
   end
 end
+
