6c2d8f2fc16b813a0f893d48dadb1db8e2de4db1
Daniel Glunz <dannyglunz@gmail.com>
2015-02-03 18:19:39 -0700

    specify which resumes to test for parser spec

--- spec/models/resume_parser_spec.rb
+++ spec/models/resume_parser_spec.rb
@@ -4,7 +4,8 @@ resume_paths = Dir.entries('./spec/resumes/').map { |e| "./spec/resumes/#{e}" }
 
 describe ResumeParser, '#pdf_to_string' do 
   it 'can convert pdf to a string object' do
-    parser = ResumeParser.new(resume_paths.last) 
+    pdf = resume_paths.find { |r| File.basename(r) == "ws-test.pdf" }
+    parser = ResumeParser.new(pdf) 
 
     expect(parser.resume).to include "Eco-Products"
   end
