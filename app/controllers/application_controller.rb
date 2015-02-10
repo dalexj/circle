5948671505d9523a86c44c6038e963fa043b4b8a
Alex <96dalex@gmail.com>
2015-02-10 07:39:32 -0700

    only look for a user if the session has a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :require_admin
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   def require_admin
